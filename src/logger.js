8b814cdd6511293a6f0940edda03f18d13e07688
VanillaChanny <39040261+VanillaChan6571@users.noreply.github.com>
2026-03-11 06:10:55 -0700

    Fix Auto Start

--- src/logger.js
+++ src/logger.js
@@ -3,10 +3,7 @@ const fs = require('fs');
 const pteroLog = (() => {
   try {
     const fd = fs.openSync('/proc/1/fd/1', 'w');
-    return (msg) => {
-      fs.writeSync(fd, msg + '\n');
-      console.log(msg);
-    };
+    return (msg) => fs.writeSync(fd, msg + '\n');
   } catch {
     return (msg) => console.log(msg);
   }
